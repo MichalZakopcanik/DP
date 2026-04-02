38760da0ec41e839d1aaf39ed8feec437a644ea2
MichalZakopcanik <99816526+MichalZakopcanik@users.noreply.github.com>
2026-04-02 17:00:18 +0200

    Updating graph and adding blank data
    Created function for updating graphs - extending traces instead of using newPlot() and adding blank data to seperate different measurements - currently in-between time of 10+ minutes adds 1 blank point. If in-between time of 60+ minutes - adds blank point for each hour.

--- airQ/public/script.js
+++ airQ/public/script.js
@@ -23,8 +23,10 @@ themeToggleButton.addEventListener("click", () => {
     }
 });
 
+
+
 // Socket.IO setup
-const socket = io("EXAMPLE_IP:EXAMPLE_PORT"); //replace with your server IP and port
+const socket = io(); //leaving empty because same origin, if different origin use io("http://yourserver:port")
 const timestampLastCard = document.getElementById("timestamp");
 const temperatureLastCard = document.getElementById("temperature");
 const humidityLastCard = document.getElementById("humidity");
@@ -44,7 +46,11 @@ const dateTimeFormat = new Intl.DateTimeFormat("sk",
         second: "2-digit"
     }); //formatting timestamp to specific locale and timezone
 
-let sessionData = [];
+let latestData = {};
+let fetchedData = [];
+let currentRange = "";
+let cutoffDate;
+let firstLoad = true;
 
 socket.on("air-update", (air_data) => {
     if (air_data.timestamp != null) {
@@ -74,13 +80,28 @@ socket.on("air-update", (air_data) => {
     if (air_data.tvoc != null) {
         tvocLastCard.innerText = `TVOC: ${air_data.tvoc} ppb`;
     }
+    latestData = air_data;
+    if (!firstLoad){
+        updateGraphs();
+    }
+    firstLoad = false;
 });
 
 async function loadGraphs(range = "hour") {
+    if (range === currentRange) return; //preventing fetching the same data again
     const response = await fetch(`/api/${range}`)
     if (response.ok) {
-       const data = await response.json();
-       createGraphs(data);     
+       fetchedData = await response.json();
+       console.log("Current range: " + currentRange);
+       console.log("Fetched range: " + range);
+       console.log("Fetched data: " + fetchedData);
+       currentRange = range;
+       fetchedData = addBlankData(fetchedData);
+       /*if (fetchedData && fetchedData.length > 0) {
+        console.log("First timestamp: " + fetchedData[0].timestamp);
+        console.log("Last timestamp: " + fetchedData[fetchedData.length-1].timestamp);
+       }*/
+       createGraphs(fetchedData, "create");     
     }
     else {
         throw new Error(`Failed to fetch historical data: ${response.status} ${response.statusText}`);
@@ -114,9 +135,56 @@ graphButton6Months.addEventListener("click", () => {
     loadGraphs('6months');
 });
 
-//using newPlot for simplicity
-//TODO: optimize for better performance
-function createGraphs(data) {
+function addBlankData(data) {
+    if(!data || data.length == 0) return data;
+    let timestamp = data.map(item => new Date(item.timestamp));
+    let modifiedData = [];
+    //console.log("Inside addBlankData");
+    for (let i = 1; i < timestamp.length; i++) {
+        let currentTime = new Date(timestamp[i]);
+        let previousTime = new Date(timestamp[i - 1]);
+        let timeDiff = (currentTime - previousTime) / (1000 * 60); //time difference in minutes
+        //console.log("Time difference between " + timestamp[i-1] + " and " + timestamp[i] + ": " + timeDiff + " minutes");
+        modifiedData.push(data[i-1]);
+        if (timeDiff > 60){ //if above 1 hour, add blank data for each hour
+            for (let j = timeDiff; j > 0; j-=60) {
+                let diffTime = new Date(currentTime);
+                diffTime.setMinutes(diffTime.getMinutes()-1*j);
+                //console.log("Added diffTime " + diffTime);
+                let blankData = {
+                    timestamp: diffTime,
+                    temperature: null,
+                    humidity: null,
+                    aqi: null,
+                    co2: null,
+                    tvoc: null,
+                    pm1: null,
+                    pm2: null,
+                    pm10: null
+                };
+                modifiedData.push(blankData);
+                //console.log("Added blank data at " + dateTimeFormat.format(blankData.timestamp));
+            }
+        } else if(timeDiff > 10){
+            let blankData = {
+                    timestamp: new Date(currentTime - timeDiff/2 * 60 * 1000),
+                    temperature: null,
+                    humidity: null,
+                    aqi: null,
+                    co2: null,
+                    tvoc: null,
+                    pm1: null,
+                    pm2: null,
+                    pm10: null
+                };
+            modifiedData.push(blankData);
+        }
+    }
+    modifiedData.push(data[data.length-1]);
+    return modifiedData;
+}
+
+function createGraphs(data, type = "create") {
     const timestamp = data.map(item => dateTimeFormat.format(new Date(item.timestamp)));
     const temperature = data.map(item => item.temperature);
     const humidity = data.map(item => item.humidity);
@@ -145,17 +213,20 @@ function createGraphs(data) {
 
     var layout = {
         title: 'Teplota a vlhkosť',
-        xaxis: {title: 'Čas'},
-        yaxis: {title: 'Teplota (°C)'},
+        xaxis: {title: {text: 'Čas'}},
+        yaxis: {title: {text:'Teplota (°C)'}},
         yaxis2: {
-            title: 'Vlhkosť (%)',
+            title: {text:'Vlhkosť (%)'},
             overlaying: 'y',
             side: 'right'
-        }
+        },
+        margin: { l: 60, r: 80, t: 20, b: 250 },
+        autosize: true
     };
 
     var graphData = [trace1, trace2];
-    Plotly.newPlot('graphDiv', graphData, layout);    
+    if (type === "create") Plotly.newPlot('graphDiv', graphData, layout); 
+    else {Plotly.react('graphDiv', graphData, layout);}   
 
     var trace1 = {
     x: timestamp,
@@ -183,23 +254,26 @@ function createGraphs(data) {
 
     var layout = {
         title: 'PM1, PM2.5 a PM10',
-        xaxis: {title: 'Čas'},
-        yaxis: {title: 'PM1 (µg/m³)'},
+        xaxis: {title: {text:'Čas'}},
+        yaxis: {title: {text:'PM1 (µg/m³)'}},
         yaxis2: {
-            title: 'PM2.5 (µg/m³)',
+            title: {text:'PM2.5 (µg/m³)'},
             overlaying: 'y',
             side: 'right'
         },
         yaxis3: {
-            title: 'PM10 (µg/m³)',
+            title: {text:'PM10 (µg/m³)'},
             overlaying: 'y',
             side: 'right',
             position: 0.95
-        }
+        },
+        margin: { l: 60, r: 80, t: 20, b: 250 },
+        autosize: true
     };
     
     var graphData = [trace1, trace2, trace3];
-    Plotly.newPlot('graphDiv2', graphData, layout);
+    if (type === "create") Plotly.newPlot('graphDiv2', graphData, layout); 
+    else {Plotly.react('graphDiv2', graphData, layout);} 
 
     var trace1 = {
     x: timestamp,
@@ -227,23 +301,65 @@ function createGraphs(data) {
     
     var layout = {
         title: 'AQI, CO2 a TVOC',
-        xaxis: {title: 'Čas'},
-        yaxis: {title: 'AQI'},
+        xaxis: {title: {text:'Čas'}},
+        yaxis: {title: {text:'AQI'}},
         yaxis2: {
-            title: 'CO2 (ppm)',
+            title: {text:'CO2 (ppm)'},
             overlaying: 'y',
             side: 'right'
         },
         yaxis3: {
-            title: 'TVOC (ppb)',
+            title: {text:'TVOC (ppb)'},
             overlaying: 'y',
             side: 'right',
             position: 0.95
-        }
+        },
+        margin: { l: 60, r: 80, t: 20, b: 250 },
+        autosize: true
     };
 
     var graphData = [trace1, trace2, trace3];
-    Plotly.newPlot('graphDiv3', graphData, layout);
+    if (type === "create") Plotly.newPlot('graphDiv3', graphData, layout); 
+    else {Plotly.react('graphDiv3', graphData, layout);} 
 }
 
-loadGraphs(); //initial load of graphs - last hour data
+function updateGraphs() {
+    const timestamp = fetchedData.map(item => new Date(item.timestamp));
+    console.log(timestamp);
+    cutoffDate = new Date();
+    switch (currentRange){
+        case "hour":
+            cutoffDate.setHours(cutoffDate.getHours() - 1);
+            break;
+        case "day":
+            cutoffDate.setDate(cutoffDate.getDate() - 1);
+            break;
+        case "week":
+            cutoffDate.setDate(cutoffDate.getDate() - 7);
+            break;
+        case "month":
+            cutoffDate.setMonth(cutoffDate.getMonth() - 1);
+            break;
+        case "6months":
+            cutoffDate.setMonth(cutoffDate.getMonth() - 6);
+            break;
+        default:
+            break;
+    }
+    if (timestamp.length > 0 && timestamp[0] < cutoffDate) {
+        fetchedData = fetchedData.filter(item => new Date(item.timestamp) >= cutoffDate);
+        createGraphs(fetchedData, "update");
+    } 
+    fetchedData.push(latestData);
+    Plotly.extendTraces('graphDiv', {   x:[[dateTimeFormat.format(new Date(latestData.timestamp))], [dateTimeFormat.format(new Date(latestData.timestamp))]], 
+                                        y: [[latestData.temperature], [latestData.humidity]]}, 
+                                        [0,1]);
+    Plotly.extendTraces('graphDiv2', {  x:[[dateTimeFormat.format(new Date(latestData.timestamp))], [dateTimeFormat.format(new Date(latestData.timestamp))], [dateTimeFormat.format(new Date(latestData.timestamp))]], 
+                                        y: [[latestData.pm1], [latestData.pm2], [latestData.pm10]]}, 
+                                        [0,1,2]);
+    Plotly.extendTraces('graphDiv3', {  x:[[dateTimeFormat.format(new Date(latestData.timestamp))], [dateTimeFormat.format(new Date(latestData.timestamp))], [dateTimeFormat.format(new Date(latestData.timestamp))]], 
+                                        y: [[latestData.aqi], [latestData.co2], [latestData.tvoc]]}, 
+                                        [0,1,2]);
+}
+
+loadGraphs(); //initial load - last hour data
